be8acc2298c8a9df71bb1b3d501245a11ef96d49
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-03-27 20:28:02 +0200

    Update minumum_subarray.cpp

--- Divide and conqure/minumum_subarray.cpp
+++ Divide and conqure/minumum_subarray.cpp
@@ -19,7 +19,46 @@ int minumum_subarray(int arr[], int size) {
 	return max;
 }
 
+//Time complexity : O(n)
+//Divide and conqure
+/*
+we emplemebt this code with three functions 
+1- Function for max value
+2- Function to get the sub-array with mid 
+3- Recursive function to get all functions
+*/
+int max(int a, int b)
+{return (a > b) ? a : b;}
 
+int MaxCrossingSum(int arr[], int low, int mid, int high)
+{
+	int sum = 0;
+	int leftpartsum = -1;
+	for (int i = mid; i >= low; i--)
+	{
+		sum = sum + arr[i];
+		if (sum > leftpartsum)
+			leftpartsum = sum;
+	}
+	sum = 0;
+	int rightpartsum = -1;
+	for (int i = mid + 1; i <= high; i++)
+	{
+		sum = sum + arr[i];
+		if (sum > rightpartsum)
+			rightpartsum = sum;
+	}
+	return leftpartsum + rightpartsum;
+}
+
+int MaxSubArraySum(int arr[], int low, int high)
+{
+	int mid;
+	if (low == high)
+		return arr[low];
+	mid = (low + high) / 2;
+	return max(max(MaxSubArraySum(arr, low, mid), MaxSubArraySum(arr, mid + 1, high)), MaxCrossingSum(arr, low, mid, high));
+}
 
 int main()
 {
